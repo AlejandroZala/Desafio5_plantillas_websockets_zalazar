8b719877842d41e93ce87fc07a58cc5a46cf1442
Alejandro Zalazar <alejandrodzalazar@gmail.con>
2023-05-01 21:32:17 -0300

    color fondos vistas

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -7,13 +7,14 @@ const manager = new ProductManager();
 router.get('/', async (req,res)=>{
     try {
         const products = await manager.getProducts();
-        res.render('products',{products})
+        res.render('products',{products, css: 'products'});
+
     } catch (error) {
         res.status(500).send({status:"error", error: "Error al obtener productos"})
     }
 })
 router.get('/realTimeProducts', async(req,res)=>{
     const products = await manager.getProducts();
-    res.render('realTimeProducts', {products});
+    res.render('realTimeProducts', {products, css: 'realTimeProducts'});
 })
 export default router;
